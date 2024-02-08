c3a1d02d83c923bde38719fa0b6035053d88add2
Hivian <toto@toto.com>
2024-02-08 14:35:33 +0100

    qbittorent

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ docker compose -f ./plex/docker-compose.yml up -d --force-recreate
 docker compose -f ./pihole/docker-compose.yml up -d --force-recreate
 docker compose -f ./nginx/docker-compose.yml up -d --force-recreate
 docker compose -f ./owncloud/docker-compose.yml up -d --force-recreate
+docker compose -f ./qbittorrent/docker-compose.yml up -d --force-recreate
