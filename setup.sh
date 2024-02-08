f99a668d70f1771034cac597f68575a8ac4c398c
Hivian <toto@toto.com>
2024-02-08 23:06:03 +0100

    qbittorent fix

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ docker compose -f ./pihole/docker-compose.yml up -d --force-recreate
 docker compose -f ./nginx/docker-compose.yml up -d --force-recreate
 docker compose -f ./owncloud/docker-compose.yml up -d --force-recreate
 docker compose -f ./qbittorrent/docker-compose.yml up -d --force-recreate
+
+# Test qbittorrent public ip
+# curl ifconfig.IO
+
+# Test gluetun connection
+# docker run --rm --network=container:gluetun alpine:3.18 sh -c \"apk add wget && wget -qO- https://ipinfo.io\"
