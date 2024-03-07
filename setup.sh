74f40f5d27e29a5ce355e09a78a139e81925a84c
Hivian <toto@toto.com>
2024-03-07 20:59:30 +0100

    Setting up radarr

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ docker compose -f ./qbittorrent/docker-compose.yml up -d --force-recreate
 docker compose -f ./portainer/docker-compose.yml up -d --force-recreate
 docker compose -f ./nginx-proxy/docker-compose.yml up -d --force-recreate
 
+# sonarr & radarr root permissions to fix root folder bug
+sudo chown -R 1000:1000 ./sonarr-radar/radarr && sudo chmod 755 -R ./sonarr-radarr/radarr
+sudo chown -R 1000:1000 ./sonarr-radar/sonarr && sudo chmod 755 -R ./sonarr-radarr/sonarr
+
 # Test qbittorrent public ip
 # curl ifconfig.IO
 
